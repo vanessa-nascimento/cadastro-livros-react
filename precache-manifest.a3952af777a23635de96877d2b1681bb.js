49ab8b8df8879f25a32de9842e0771ffc4a8300d
Vanessa Nascimento <vanascimento@uolinc.com>
2020-05-20 22:03:20 -0300

    Updates

---
+++ precache-manifest.a3952af777a23635de96877d2b1681bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2db0be301602865f0615",
+    "url": "/static/js/main.de87d192.chunk.js"
+  },
+  {
+    "revision": "6da1c190530c992cbece",
+    "url": "/static/js/2.1421eefc.chunk.js"
+  },
+  {
+    "revision": "2db0be301602865f0615",
+    "url": "/static/css/main.ed8e004a.chunk.css"
+  },
+  {
+    "revision": "6da1c190530c992cbece",
+    "url": "/static/css/2.285d38a1.chunk.css"
+  },
+  {
+    "revision": "0800665a2356dcbc6622e8c965904add",
+    "url": "/index.html"
+  }
+];
